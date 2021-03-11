eacf7c434fdaac54a7b391e457381a0b82a32ecf
Jean <46204992+JeanCarlosVR@users.noreply.github.com>
2021-03-10 21:12:43 -0500

    Add files via upload

---
+++ src/api/HTTP.ts
@@ -0,0 +1,43 @@
+import Axios, { AxiosPromise } from 'axios';
+
+export const restVersion: number = 1;
+export const restURL: string = `http://127.0.0.1:5000`;
+export const restEndpoints: any = {
+    main: {
+        _: `/api/v${restVersion}`
+    },
+    data: {
+        get: {
+            getUserByID: `/api/v${restVersion}/get/me/user` // :id
+        }
+    }
+}
+
+/**
+ * 
+ * Cambiar AXIOS por otro que pase en JSON y no en string, golang lee json, NO STRINGS
+ */
+export async function getUser(token: string, id: string): Promise<any> {
+    const _r: AxiosPromise<any> | any = await Axios({
+        method: "POST",
+        url: `${restURL}${restEndpoints.data.get.getUserByID}/${id}`,
+        data: JSON.stringify({
+            token,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "include",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Accept, Accept-Language, Content-Type"
+        }
+    });
+
+    // eslint-disable-next-line
+    if(!_r || (_r && !_r.data || (_r.data && _r.data.exited_code !== 0 || !(_r.data.data)))) {
+        return null;
+    }
+
+    window.sessionStorage.setItem("camxa-oauth-session-data", JSON.stringify(_r.data.data));
+
+    return _r.data.data;
+}

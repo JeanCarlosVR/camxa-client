c9272f4b2f1a73012d89fdc0af75d54de785e402
Jean <46204992+JeanCarlosVR@users.noreply.github.com>
2021-03-10 21:14:14 -0500

    Add files via upload

---
+++ src/assets/images/Images.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+export default {
+    "main": {
+        "logo": {
+            "_": "https://firebasestorage.googleapis.com/v0/b/jean-services.appspot.com/o/camxa_logo.png?alt=media&token=21118853-2016-4f7a-81cd-25487f7f52cd",
+            64: "",
+            124: "",
+            256: "",
+            512: "",
+            1024: "",
+            2048: ""
+        }
+    }
+}
